c62a3156dc20a97d73aae341d3460c790e747de0
Vincentyun <vincentzyc@qq.com>
2019-03-22 19:14:46 +0800

    登录界面样式优化

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="login-wrap">
-    <div class="ms-title">管理系统</div>
-    <div class="ms-login">
-      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px">
-        <el-form-item prop="userName">
-          <el-input v-model="ruleForm.userName" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item prop="passWord">
-          <el-input
-            type="password"
-            placeholder="请输入密码"
-            v-model="ruleForm.passWord"
-            @keyup.enter.native="submitForm('ruleForm')"
-          ></el-input>
-        </el-form-item>
-        <p style="color:#999;font-size:12px;margin:-10px 0 10px 0">用户名密码随意输入即可</p>
-        <div class="login-btn">
-          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-        </div>
-      </el-form>
+    <div class="login-bg"></div>
+    <div class="login-form">
+      <h1 class="title">管理系统</h1>
+      <div class="form">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px">
+          <el-form-item prop="userName">
+            <el-input v-model="ruleForm.userName" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item prop="passWord">
+            <el-input type="password" placeholder="请输入密码" v-model="ruleForm.passWord" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+          </el-form-item>
+          <p style="color:#999;font-size:12px;margin:-5px 0 10px 0">用户名密码随意输入即可</p>
+          <div class="login-btn-wrap">
+            <el-button class="login-btn" type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          </div>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function() {
+  data: function () {
     return {
       nextUrl: "",
       ruleForm: {
@@ -71,35 +69,43 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
+}
+.login-bg {
+  z-index: -99;
+  position: absolute;
+  width: 100%;
+  height: 100%;
   background: #324157;
 }
-
-.ms-title {
-  position: absolute;
-  top: 50%;
-  width: 100%;
-  margin-top: -230px;
-  text-align: center;
-  font-size: 30px;
-  color: #fff;
-}
-
-.ms-login {
+.login-form {
+  z-index: 99;
   position: absolute;
   left: 50%;
-  top: 50%;
-  width: 380px;
-  margin: -150px 0 0 -190px;
+  top: 40%;
+  transform: translate(-50%, -50%);
+}
+.title {
+  text-align: center;
+  font-size: 50px;
+  color: #fff;
+  margin-bottom: 30px;
+  background-image: -webkit-linear-gradient(92deg, #d1451a, #ff9300);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.form {
+  width: 360px;
   padding: 40px;
-  border-radius: 5px;
-  background: #fff;
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.1);
+}
+
+.login-btn-wrap {
+  text-align: center;
 }
 
 .login-btn {
-  text-align: center;
-}
-
-.login-btn button {
   width: 100%;
   height: 36px;
 }
