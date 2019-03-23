a1ce4331d628972c818e3e6b12ae72ee6875d22b
vincentzyc <vincentzyc@qq.com>
2019-03-23 23:57:53 +0800

    fix core-js module error  -- vue-cli/issues/3678

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  publicPath: "./",
-  outputDir: "docs",
-  productionSourceMap: false,
-
-  devServer: {
-    port: 3010
-  }
+	publicPath: "./",
+	outputDir: "docs",
+	productionSourceMap: false,
+	devServer: {
+		port: 3010
+	}
 };
