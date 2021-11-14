155824bf4837ca75a70826cc30c8abd65f8f68ed
vincentzyc <vincentzyc@qq.com>
2021-11-14 19:03:00 +0800

    🎉 init: vue3 init

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,26 @@ module.exports = {
 	publicPath: "./",
 	outputDir: "docs",
   productionSourceMap: false,
+  // configureWebpack: {
+  //   // 以下库使用cdn，不会被打包
+  //   externals: process.env.NODE_ENV === 'production' ? {
+  //     "vue": "Vue",
+  //     "vuex": "Vuex",
+  //   } : {}
+  // },
+
+  configureWebpack: {
+    plugins: [
+      require('unplugin-vue-components/webpack')({ /* options */ }),
+    ],
+  },
+
   chainWebpack: config => {
+    config.plugins.delete('preload')
     config.plugins.delete('prefetch')
   },
-	devServer: {
-		port: 3010
-	}
+
+  devServer: {
+    port: 8060
+  }
 };
