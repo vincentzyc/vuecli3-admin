b29920405ff57a0d2e6305f214ca35ab599892c0
vincentzyc <vincentzyc@qq.com>
2021-11-16 09:01:04 +0800

    🎉 init: vue3 init

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'element-plus/dist/index.css'
+import './assets/css/main.styl'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.config.globalProperties.BASE_URL = process.env.BASE_URL
+
+app.use(store).use(router).mount('#app')
