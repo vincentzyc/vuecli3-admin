1f17a58925aab069ed84c38d28f94ad146b8c58f
vincentzyc <vincentzyc@qq.com>
2021-11-18 09:40:08 +0800

    🎉 init: vue3 init

---
+++ src/views/About/About1.vue
@@ -0,0 +1,14 @@
+<template>
+  <BasePage :paths="['about', 'about1']" showBack @handle-back="router.back()">
+    <div class="jb-content">
+      <h1>about1</h1>
+    </div>
+  </BasePage>
+</template>
+
+<script lang="ts" setup>
+import BasePage from "@/components/BasePage/index.vue";
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
---
+++ src/views/About/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <router-view v-slot="{ Component }">
+      <transition name="el-fade-in" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>
