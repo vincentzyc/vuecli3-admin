03a00513d4f5814a9b607fcc1e5d4b8fbcfebe43
vincentzyc <vincentzyc@qq.com>
2021-11-19 09:37:59 +0800

    🎉 init: init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,11 @@
 <template>
-  <BasePage :paths="[111, 222]" showBack @handle-back="router.back()">
+  <BasePage :paths="[111, 222]">
     <div class="jb-content">
-      <h1>asdfasdf</h1>
+      <h1>home</h1>
     </div>
   </BasePage>
 </template>
 
 <script lang="ts" setup>
-import BasePage from "@/components/BasePage/index.vue";
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
+import BasePage from "@/components/BasePage/index.vue"
 </script>
