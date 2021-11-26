c88e2ecc78126d30463b127e48860aeb26cdf6bc
vincentzyc <vincentzyc@qq.com>
2021-11-26 09:17:03 +0800

    🎉 init: init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
 import 'element-plus/dist/index.css'
+import './assets/css/base.css'
 import './assets/css/main.styl'
 
 const app = createApp(App);
