689dce31147cc6466075ba21b0281e578d8f54f1
vincentzyc <vincentzyc@qq.com>
2021-11-26 09:17:11 +0800

    🎉 init: init

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="welcome-wrap" style="height:100%">
-    <img :src="BASE_URL + 'static/img/logo.png'" alt="logo" class="junbo-logo" />
-    <span class="junbo-value">创造价值 分享价值</span>
+    <img :src="BASE_URL + 'img/logo.png'" alt="logo" class="yunyi-logo" />
+    <span class="yunyi-value">创造价值 分享价值</span>
     <div class="welcome-text">Welcome</div>
-    <img :src="BASE_URL + 'static/img/welcome.jpg'" alt="欢迎" style="width:100%" />
+    <img :src="BASE_URL + 'img/welcome.jpg'" alt="欢迎" style="width:100%" />
   </div>
 </template>
