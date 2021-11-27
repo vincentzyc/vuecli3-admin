18616433b1fa44423908e516d884ea884fb13ac6
vincentzyc <vincentzyc@qq.com>
2021-11-27 13:44:30 +0800

    🎉 init: init

--- src/views/About/About1.vue
+++ src/views/About/About1.vue
@@ -1,5 +1,5 @@
 <template>
-  <BasePage :paths="['about', 'about1']" showBack @handle-back="router.back()">
+  <BasePage :paths="breadcrumb" showBack @handle-back="router.back()">
     <div class="jb-content">
       <h1>about1</h1>
     </div>
@@ -7,8 +7,15 @@
 </template>
 
 <script lang="ts" setup>
-import BasePage from "@/components/BasePage/index.vue";
+import { useStore } from "@/store";
+import { computed } from "vue";
 import { useRouter } from 'vue-router'
+import BasePage from "@/components/BasePage/index.vue"
 
+const store = useStore()
 const router = useRouter()
+const breadcrumb = computed(() => store.state.common.breadcrumb)
+
+store.commit('setBreadcrumb', ['about', 'about1'])
+
 </script>
