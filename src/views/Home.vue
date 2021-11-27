0ab63a84028b70b6cb4c9ddf78de4647b6337cc2
vincentzyc <vincentzyc@qq.com>
2021-11-27 13:42:12 +0800

    🎉 init: init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <BasePage :paths="[111, 222]">
+  <BasePage :paths="breadcrumb">
     <div class="jb-content">
       <h1>home</h1>
     </div>
@@ -7,5 +7,14 @@
 </template>
 
 <script lang="ts" setup>
+import { useStore } from "@/store";
 import BasePage from "@/components/BasePage/index.vue"
+import { computed } from "@vue/reactivity";
+
+const store = useStore()
+
+const breadcrumb = computed(() => store.state.common.breadcrumb)
+
+store.commit('setBreadcrumb', ['111', '222'])
+
 </script>
