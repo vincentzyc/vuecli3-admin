b010ecc71e71e5e84b5cb1310259f493d10166e7
vincentzyc <vincentzyc@qq.com>
2021-12-02 15:59:34 +0800

    🎉 init: init

--- src/utils/validate/date.ts
+++ src/utils/validate/date.ts
@@ -2,8 +2,5 @@ import { isNaN } from './number';
 import { getType } from './dataType'
 
 export function isDate(val: Date): val is Date {
-  return (
-    getType(val) === 'Date' &&
-    !isNaN(val.getTime())
-  );
+  return getType(val) === 'Date' && !isNaN(val.getTime())
 }
