b48ae12921927f82358cf3e3f3ab9b939ee7983a
vincentzyc <vincentzyc@qq.com>
2021-12-04 19:10:46 +0800

    🎉 init: init

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { ElConfigProvider } from 'element-plus'
-
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
 export default defineComponent({
   name: "app",
   components: {
