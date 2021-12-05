fe84d3c98fb477e692d44f45434dd4653c05890d
vincentzyc <vincentzyc@qq.com>
2021-12-05 16:32:23 +0800

    🎉 init: init

--- vue.config.js
+++ vue.config.js
@@ -10,20 +10,12 @@ module.exports = {
       "element-plus": "ElementPlus"
     } : {}
   },
-
-  // configureWebpack: {
-  //   plugins: [
-  //     require('unplugin-vue-components/webpack')({ /* options */ }),
-  //   ],
-  // },
-
   chainWebpack: config => {
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
   },
-
   devServer: {
     port: 3060,
-    proxy: 'http://test-cms-admin.jetmobo.com'
+    proxy: 'http://xxx.com'
   }
-};
+}
