512be32e1fda64fa28a548af55116b07757b67a3
vincentzyc <vincentzyc@qq.com>
2021-12-07 08:58:27 +0800

    🎉 init: init

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { ElConfigProvider } from 'element-plus'
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
 export default defineComponent({
   name: "app",
-  components: {
-    ElConfigProvider,
-  },
   setup() {
     return {
       locale: zhCn,
