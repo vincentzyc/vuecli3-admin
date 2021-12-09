45d4b73ca203f31677759e2b7c3280ad5fc43360
vincentzyc <vincentzyc@qq.com>
2021-12-09 10:02:47 +0800

    ✨ feat: 新增获取图片分辨率

--- src/utils/image-info.ts
+++ src/utils/image-info.ts
@@ -3,8 +3,11 @@ export async function getImageInfo(imageFile: File) {
   const image = new Image()
   const { name, size, type } = imageFile
   image.src = imageSrc
-  return new Promise(resolve => {
-    image.onload = () => {
+  return new Promise((resolve, reject) => {
+    image.addEventListener('error', error => {
+      reject(error)
+    })
+    image.addEventListener('load', () => {
       resolve({
         width: image.width,
         height: image.height,
@@ -13,6 +16,6 @@ export async function getImageInfo(imageFile: File) {
         type,
         imageFile
       })
-    }
+    })
   })
 }
--- src/views/About/About1.vue
+++ src/views/About/About1.vue
@@ -1,21 +1,42 @@
 <template>
   <BasePage :paths="breadcrumb" showBack @handle-back="router.back()">
     <div class="jb-content">
-      <h1>about1</h1>
+      <h1>获取图片分辨率</h1>
+      <input type="file" id="file" @change="imageInfo" accept="image/*" />
+      <p>{{ JSON.stringify(info) }}</p>
     </div>
   </BasePage>
 </template>
 
 <script lang="ts" setup>
 import { useStore } from "@/store";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useRouter } from 'vue-router'
 import BasePage from "@/components/BasePage/index.vue"
+import { getImageInfo } from "@/utils/image-info";
 
 const store = useStore()
 const router = useRouter()
 const breadcrumb = computed(() => store.state.common.breadcrumb)
 
 store.commit('setBreadcrumb', ['about', 'about1'])
+
+const info = ref()
+
+function imageInfo() {
+  const elFiles = document.getElementById('file') as HTMLInputElement
+  if (elFiles) {
+    if (elFiles.files && elFiles.files.length > 0) {
+      for (let i = 0; i < elFiles.files.length; i++) {
+        const file = elFiles.files[i]
+        getImageInfo(file).then(obj => {
+          info.value = obj
+        }).catch((error: any) => {
+          console.log(error)
+        })
+      }
+    }
+  }
+}
 
 </script>
