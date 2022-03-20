d7e46b8020fe9b6777fe83c0e28c90b7f19af654
vincentzyc <vincentzyc@qq.com>
2022-03-20 15:13:24 +0800

    🎉 init: pinia init

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
+import { createPinia } from 'pinia'
 import 'element-plus/dist/index.css'
 import './assets/css/base.css'
 import './assets/css/main.styl'
 
 const app = createApp(App);
+
+app.use(createPinia())
 
 app.config.globalProperties.BASE_URL = process.env.BASE_URL
 
